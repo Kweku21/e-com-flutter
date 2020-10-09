51c0ce8fd63e877e269ce3ba1088e0e6fdc4f5d4
Kweku Debrah <edebrah@npontu.com>
2020-10-09 14:04:06 +0000

    splash

---
+++ lib/components/default_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../size_config.dart';
+
+
+class DefualtButton extends StatelessWidget {
+  const DefualtButton({
+    Key key,
+    @required this.text,
+    @required this.press,
+  }) : super(key: key);
+
+  final String text;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: getProportionateScreenHeight(56),
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20)
+        ),
+        color: kPrimaryColor,
+        onPressed:press,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(18),
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -3,11 +3,15 @@ import 'package:shop_app/screens/splash/components/body.dart';
 import 'package:shop_app/size_config.dart';
 
 class SplashScreen extends StatefulWidget {
+
+  static String routeName ="/splash";
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+
   @override
   Widget build(BuildContext context) {
     // You gave to call it on your starting page
